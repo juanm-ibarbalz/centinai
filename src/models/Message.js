7c9379f8a3e2d26446410ae5b58ba968beffa2c4
Parkizan <juanmartin.ibarbalz@gmail.com>
2025-04-19 18:56:43 -0300

    Agregar lógica de filtrado básico y clasificación de mensajes + mejoras en el esquema

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,14 +2,33 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    conversationId: String,
-    timestamp: String,
-    from: String,
-    text: String,
-    type: String,
-    raw: Object,
+    messageId: String, // ID del mensaje
+    conversationId: String, // ID de la conversación
+    timestamp: String, // Fecha y hora de la creación
+    from: String, // ID de quien envió el mensaje
+    userName: String, // Nombre del usuario
+    text: String, // Contenido del mensaje
+    type: String, // Tipo de mensaje
+    direction: {
+      type: String,
+      enum: ["user", "agent"],
+    }, // De parte de quien es el mensaje
+    status: {
+      type: String,
+      default: "active",
+    }, // Estado del mensaje/conversación
   },
-  { timestamps: true },
+  {
+    timestamps: true, // esto agrega createdAt y updatedAt automáticamente
+  },
 );
+
+// Índices
+messageSchema.index({ messageId: 1 });
+messageSchema.index({ conversationId: 1 });
+messageSchema.index({ from: 1 });
+messageSchema.index({ timestamp: 1 });
+messageSchema.index({ type: 1 });
+messageSchema.index({ status: 1 });
 
 export default mongoose.model("Message", messageSchema);
--- src/services/messageProcessor.js
+++ src/services/messageProcessor.js
@@ -2,17 +2,33 @@ import Message from "../models/Message.js";
 
 export const saveIncomingMessage = async (body) => {
   const entry = body?.entry?.[0];
-  const changes = entry?.changes?.[0]?.value?.messages?.[0];
+  const change = entry?.changes?.[0];
+  const message = change?.value?.messages?.[0];
+  const contact = change?.value?.contacts?.[0];
 
-  if (!changes) return;
+  if (!message) return;
 
+  // 1. Filtrar mensajes vacíos
+  if (!message.text?.body) return;
+
+  // 2. Filtrar todo lo que no sea tipo "text"
+  if (message.type !== "text") return;
+
+  // 3. Determinar dirección del mensaje
+  const isUser = message.from === contact?.wa_id;
+  const direction = isUser ? "user" : "agent";
+
+  // 4. Armar documento
   const messageData = {
+    messageId: message.id,
     conversationId: entry.id,
-    timestamp: changes.timestamp,
-    from: changes.from,
-    text: changes.text?.body,
-    type: changes.type,
-    raw: changes,
+    timestamp: message.timestamp,
+    from: message.from,
+    userName: contact?.profile?.name || "Desconocido",
+    text: message.text.body,
+    type: message.type,
+    direction,
+    status: "active",
   };
 
   await Message.create(messageData);
