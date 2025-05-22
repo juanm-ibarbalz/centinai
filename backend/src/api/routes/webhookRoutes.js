b64edf1951e537ada33db562283acd7a5314b6d0
Parkizan <juanmartin.ibarbalz@gmail.com>
2025-05-22 16:33:34 -0300

    fix: remove old webhook verify import

--- backend/src/api/routes/webhookRoutes.js
+++ backend/src/api/routes/webhookRoutes.js
@@ -1,8 +1,5 @@
 import express from "express";
-import {
-  verifyWebhook,
-  handleIncomingMessage,
-} from "../controllers/webhookController.js";
+import { handleIncomingMessage } from "../controllers/webhookController.js";
 
 const router = express.Router();
 
